9d4f437554411f172d22e05108413f617bea93c3
pierre <pierre.bultez@proton.me>
2026-01-11 17:43:32 +0100

    Add order email notifications: customer confirmation and admin alert.

---
+++ resources/views/emails/orders/admin_notification.blade.php
@@ -0,0 +1,32 @@
+<x-mail::message>
+# Nouvelle commande #{{ $order->id }}
+
+Une nouvelle commande vient d'arriver !
+
+## Client
+**Nom :** {{ $order->customer_name }}  
+**Email :** {{ $order->customer_email }}  
+**Téléphone :** {{ $order->customer_phone }}  
+
+## Commande
+**Heure de retrait :** {{ $order->pickup_time->format('d/m/Y à H:i') }}  
+**Type :** {{ $order->delivery_method === 'delivery' ? 'LIVRAISON' : 'A EMPORTER' }}  
+@if($order->delivery_method === 'delivery')
+**Adresse :** {{ $order->customer_address }}
+@endif
+**Paiement :** {{ ucfirst($order->payment_method) }} ({{ $order->status }})
+
+<x-mail::table>
+| Article | Qté | Prix | Notes |
+| :--- | :---: | :---: | :--- |
+@foreach($order->items as $item)
+| **{{ $item->product ? $item->product->name : 'Menu' }}** @if($item->components)<br><small>{{ implode(', ', $item->components) }}</small>@endif | {{ $item->quantity }} | {{ number_format($item->unit_price * $item->quantity, 2) }} € | {{ $item->notes }} |
+@endforeach
+| **Total** | | **{{ number_format($order->total_amount, 2) }} €** | |
+</x-mail::table>
+
+<x-mail::button :url="route('admin.orders.index')">
+Gérer la commande
+</x-mail::button>
+
+</x-mail::message>
